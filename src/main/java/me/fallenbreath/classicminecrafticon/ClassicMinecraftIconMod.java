40c2d98fb3606ffc92b1fb83b0892c59fe8b8008
Fallen_Breath <yqx791125833@gmail.com>
2023-06-11 18:18:11 +0800

    dependencies cleanup

--- src/main/java/me/fallenbreath/classicminecrafticon/ClassicMinecraftIconMod.java
+++ src/main/java/me/fallenbreath/classicminecrafticon/ClassicMinecraftIconMod.java
@@ -21,26 +21,12 @@
 package me.fallenbreath.classicminecrafticon;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.api.metadata.ModMetadata;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class ClassicMinecraftIconMod implements ModInitializer
 {
-	public static final Logger LOGGER = LogManager.getLogger();
-
-	public static final String MOD_ID = "classic-minecraft-icon";
-	public static String MOD_VERSION = "unknown";
-	public static String MOD_NAME = "unknown";
-
 	@Override
 	public void onInitialize()
 	{
-		ModMetadata metadata = FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow(RuntimeException::new).getMetadata();
-		MOD_NAME = metadata.getName();
-		MOD_VERSION = metadata.getVersion().getFriendlyString();
-
 		ClassMinecraftIconStorage.init();
 	}
 }
